2a6f12a04889dd995d47d39dfa2891e2e2805d37
Miguel Diaz <61856414+seifer-d@users.noreply.github.com>
2020-12-17 17:16:18 -0500

    Update database.go

--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetDatabase() *gorm.DB {
-	dsn := "root:@tcp(127.0.0.1:3306)/app_crud?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:12345678@tcp(127.0.0.1:3306)/app_crud?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
